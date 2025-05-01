eccbbc6e802093cf147e257d9a8c18c069d4b01b
giopdev <giovanni.p.paladino@gmail.com>
2025-05-01 05:24:32 -0700

    forgot to push some stuff

---
+++ public/login.js
@@ -0,0 +1,25 @@
+/*
+ * Script for login button to hit /login endpoint and create a user session
+ */
+window.onload = () => {
+  document.getElementById('login_button').onclick = async function() {
+    const username = login_username.value;
+    const password = login_password.value;
+
+    // Hit the /login endpoint with the user's information
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ login_username: username, login_password: password }),
+    });
+    const responseBody = await response.json();
+
+    // If endpoint responds with success, redirect user to the account page
+    if (responseBody.success) { location.href = '/account'; }
+    else {
+      // Else if the response errors, set the appropriate error_label text
+      document.getElementById('error_label').textContent = responseBody.error;
+
+    }
+  };
+}
